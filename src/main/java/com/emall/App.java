d4281374be42772d6e2c61637de4b6ada1c8dc53
zrj <614266960@qq.com>
2019-02-11 22:32:08 +0800

    中文乱码

--- src/main/java/com/emall/App.java
+++ src/main/java/com/emall/App.java
@@ -1,25 +1,37 @@
 package com.emall;
 
+import com.emall.dao.UserDOMapper;
+import com.emall.dataObject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
- *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.emall"})
 @RestController
-public class App 
-{
+@MapperScan("com.emall.dao")
+public class App {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
-    public String home(){
-        return "Hello World!";
+    public String home() {
+        UserDO userDO = new UserDO();
+        userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO==null){
+            return "用户不存在";
+        }else{
+            return userDO.getName();
+        }
     }
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        SpringApplication.run(App.class,args);
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
 }
